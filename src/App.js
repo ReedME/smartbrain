f77cba9a5a567022d096651a7a766b80c0cb91f3
Brendan Reed <brendanreed@Brendans-iMac.local>
2018-01-31 21:20:42 +1100

    started building functionality into form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import './App.css';
 
+
+// set particles for particle background
 const particlesOptions =
 {
   particles: {
@@ -20,6 +22,21 @@ const particlesOptions =
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,7 +47,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit} />
 
       </div>
     );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div >
       <p className='f3'>
@@ -10,8 +10,8 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='center form pa4 shadow-5'>
-          <input className='f4 pa2 ma1 w-70 center' type='text' />
-          <button className='f4 ma1 w-30 grow link ph3 pv2 dib white bg-light-purple'> Detect Faces </button>
+          <input className='f4 pa2 ma1 w-70 center' type='text' onChange={onInputChange} />
+          <button className='f4 ma1 w-30 grow link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit} > Detect Faces </button>
         </div>
         </div>
       </div>
