2277658c893cae93536b58c3907e0fa8c5ca7f98
Brendan Reed <brendanreed@Brendans-iMac.local>
2018-01-31 16:02:20 +1100

    Added logo, nav and imagelinkform components

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const ImageLinkForm = () => {
+  return (
+    <div >
+      <p className='f3'>
+        {'This application is able to detect faces in your pictures!'}
+      </p>
+      <div>
+        <input className='f3' type='text' />
+        <button> Detect Faces </button>
+      </div>
+      </div>
+  )
+}
+
+export default ImageLinkForm
